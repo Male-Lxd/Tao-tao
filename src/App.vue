176734e3fea7e3c51c9029a9b46d4e9dbdcc37f0
Miss-Lxd <1546486048@qq.com>
2018-08-23 12:59:13 +0800

    hover效果

--- src/App.vue
+++ src/App.vue
@@ -703,11 +703,33 @@
 </template>
 
 <script>
+  import $ from "jquery";
   export default {
     name: 'app',
   }
+
+//插件
+$(document).ready(function() {
+	$("#menu2 li a").wrapInner( '<span class="out"></span>' );
+	$("#menu2 li a").each(function() {
+		$( '<span class="over">' +  $(this).text() + '</span>' ).appendTo( this );
+	});
+
+	$("#menu2 li a").hover(function() {
+		$(".out",	this).stop().animate({'top':	'48px'},	300); // move down - hide
+		$(".over",	this).stop().animate({'top':	'0px'},		300); // move down - show
+
+	}, function() {
+		$(".out",	this).stop().animate({'top':	'0px'},		300); // move up - show
+		$(".over",	this).stop().animate({'top':	'-48px'},	300); // move up - hide
+	});
+
+});
+
+
 </script>
 
 <style>
   @import url('./assets/statics/site/css/style.css');
+  @import url('./assets/lib/css/style.css');
 </style>
